6309c23be825310a6a8b3825b767152a97395289
Phlam Sicusa <sicusa@gilatod.art>
2022-10-04 23:11:15 +0800

    set default parent ID to GLRenderer.RootID

--- Aeco.Renderer.GL/Components/States/Parent.cs
+++ Aeco.Renderer.GL/Components/States/Parent.cs
@@ -5,7 +5,9 @@ using System.Runtime.Serialization;
 [DataContract]
 public struct Parent : IGLReactiveObject, IDisposable
 {
-    [DataMember] public Guid Id;
+    [DataMember] public Guid Id = GLRenderer.RootId;
+
+    public Parent() {}
 
     public void Dispose() { this = new(); }
 }
