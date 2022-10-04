b943999221d8c526f7a4f6c4b1a3cf513b0a36e5
Phlam Sicusa <sicusa@gilatod.art>
2022-10-05 00:18:01 +0800

    remove unnecessary code for WorldViewStorage

--- Aeco.Renderer.GL/Layers/Storages/WorldViewStorage.cs
+++ Aeco.Renderer.GL/Layers/Storages/WorldViewStorage.cs
@@ -20,57 +20,6 @@ public class WorldViewStorage : DelayedReactiveStorageBase<WorldView, WorldViewD
         appliedVectors.Forward = view.Forward;
     }
 
-    private static Quaternion QuaternionLookRotation(in Vector3 forward, in Vector3 up, in Vector3 right)
-    {
-        var m00 = right.X;
-        var m01 = right.Y;
-        var m02 = right.Z;
-        var m10 = up.X;
-        var m11 = up.Y;
-        var m12 = up.Z;
-        var m20 = forward.X;
-        var m21 = forward.Y;
-        var m22 = forward.Z;
-
-
-        float num8 = (m00 + m11) + m22;
-        var quaternion = new Quaternion();
-        if (num8 > 0f) {
-            var num = MathF.Sqrt(num8 + 1f);
-            quaternion.W = num * 0.5f;
-            num = 0.5f / num;
-            quaternion.X = (m12 - m21) * num;
-            quaternion.Y = (m20 - m02) * num;
-            quaternion.Z = (m01 - m10) * num;
-            return quaternion;
-        }
-        if ((m00 >= m11) && (m00 >= m22)) {
-            var num7 = MathF.Sqrt(((1f + m00) - m11) - m22);
-            var num4 = 0.5f / num7;
-            quaternion.X = 0.5f * num7;
-            quaternion.Y = (m01 + m10) * num4;
-            quaternion.Z = (m02 + m20) * num4;
-            quaternion.W = (m12 - m21) * num4;
-            return quaternion;
-        }
-        if (m11 > m22) {
-            var num6 = MathF.Sqrt(((1f + m11) - m00) - m22);
-            var num3 = 0.5f / num6;
-            quaternion.X = (m10+ m01) * num3;
-            quaternion.Y = 0.5f * num6;
-            quaternion.Z = (m21 + m12) * num3;
-            quaternion.W = (m20 - m02) * num3;
-            return quaternion; 
-        }
-        var num5 = MathF.Sqrt(((1f + m22) - m00) - m11);
-        var num2 = 0.5f / num5;
-        quaternion.X = (m20 + m02) * num2;
-        quaternion.Y = (m21 + m12) * num2;
-        quaternion.Z = 0.5f * num5;
-        quaternion.W = (m01 - m10) * num2;
-        return quaternion;
-    }
-
     protected override void OnModified(Guid id, ref WorldView view)
     {
         ref var appliedVectors = ref Context.Acquire<AppliedWorldVectors>(id);
