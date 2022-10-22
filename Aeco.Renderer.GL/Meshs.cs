33c7b5ff51e14784dcf1c2b60c32ec3376fb59ec
Phlam Sicusa <sicusa@gilatod.art>
2022-10-22 19:37:01 +0800

    remove unused file

--- Aeco.Renderer.GL/Meshs.cs
+++
@@ -1,7 +0,0 @@
-namespace Aeco.Renderer.GL;
-
-public static class Polygons
-{
-    public static Guid QuadId { get; } = Guid.Parse("16ead0af-9a4a-461d-8a7e-6b377d8569fc");
-    public static Guid CubeId { get; } = Guid.Parse("1f339164-2631-48dc-b017-527a5b5a0dd3");
-}
