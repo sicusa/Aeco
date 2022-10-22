87edfbe6c75ee50fc7324b17d726d95c36c39275
Phlam Sicusa <sicusa@gilatod.art>
2022-10-22 22:18:34 +0800

    refactor buffer initialization

--- Aeco.Renderer.GL/Layers/Objects/ResourceManagers/MeshManager.cs
+++ Aeco.Renderer.GL/Layers/Objects/ResourceManagers/MeshManager.cs
@@ -84,15 +84,8 @@ public class MeshManager : ResourceManagerBase<Mesh, MeshData, MeshResource>
     public static void InitializeInstanceBuffer(BufferTarget target, int handle, ref MeshData data)
     {
         GL.BindBuffer(target, handle);
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
-            GL.BufferData(target, data.InstanceCapacity * MeshInstance.MemorySize, IntPtr.Zero, BufferUsageHint.StaticDraw);
-        }
-        else {
-            GL.BufferStorage(target,
-                data.InstanceCapacity * MeshInstance.MemorySize, IntPtr.Zero,
-                BufferStorageFlags.MapWriteBit | BufferStorageFlags.DynamicStorageBit | BufferStorageFlags.MapPersistentBit | BufferStorageFlags.MapCoherentBit);
-        }
-        data.InstanceBufferPointer = GL.MapBuffer(BufferTarget.ArrayBuffer, BufferAccess.WriteOnly);
+        data.InstanceBufferPointer = GLHelper.InitializeBuffer(
+            target, data.InstanceCapacity * MeshInstance.MemorySize);
     }
 
     public static void InitializeInstanceCulling(ref MeshData data)
