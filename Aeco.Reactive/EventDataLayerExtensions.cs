1624c9247e07ead8f7db7b14c4379102712ad867
Phlam Sicusa <sicusa@gilatod.art>
2023-02-06 13:37:34 +0800

    add EventDataLayerExtensions

---
+++ Aeco.Reactive/EventDataLayerExtensions.cs
@@ -0,0 +1,47 @@
+namespace Aeco.Reactive;
+
+public static class EventDataLayerExtensions
+{
+    public static void MarkModified<TComponent>(this IExpandableDataLayer<IComponent> dataLayer, Guid id)
+        where TComponent : IComponent
+    {
+        dataLayer.Acquire<AnyModified<TComponent>>(ReactiveCompositeLayer.AnyEventId);
+        dataLayer.Acquire<Modified<TComponent>>(id);
+    }
+
+    public static void MarkAnyModified<TComponent>(this IExpandableDataLayer<IComponent> dataLayer)
+        where TComponent : IComponent
+    {
+        dataLayer.Acquire<AnyModified<TComponent>>(ReactiveCompositeLayer.AnyEventId);
+    }
+
+    public static void MarkCreated<TComponent>(this IExpandableDataLayer<IComponent> dataLayer, Guid id)
+        where TComponent : IComponent
+    {
+        dataLayer.Acquire<AnyCreated<TComponent>>(ReactiveCompositeLayer.AnyEventId);
+        dataLayer.Acquire<AnyCreatedOrRemoved<TComponent>>(ReactiveCompositeLayer.AnyEventId);
+        dataLayer.Acquire<Created<TComponent>>(id);
+    }
+
+    public static void MarkAnyCreated<TComponent>(this IExpandableDataLayer<IComponent> dataLayer)
+        where TComponent : IComponent
+    {
+        dataLayer.Acquire<AnyCreated<TComponent>>(ReactiveCompositeLayer.AnyEventId);
+        dataLayer.Acquire<AnyCreatedOrRemoved<TComponent>>(ReactiveCompositeLayer.AnyEventId);
+    }
+    
+    public static void MarkRemoved<TComponent>(this IExpandableDataLayer<IComponent> dataLayer, Guid id)
+        where TComponent : IComponent
+    {
+        dataLayer.Acquire<AnyRemoved<TComponent>>(ReactiveCompositeLayer.AnyEventId);
+        dataLayer.Acquire<AnyCreatedOrRemoved<TComponent>>(ReactiveCompositeLayer.AnyEventId);
+        dataLayer.Acquire<Removed<TComponent>>(id);
+    }
+
+    public static void MarkAnyRemoved<TComponent>(this IExpandableDataLayer<IComponent> dataLayer)
+        where TComponent : IComponent
+    {
+        dataLayer.Acquire<AnyRemoved<TComponent>>(ReactiveCompositeLayer.AnyEventId);
+        dataLayer.Acquire<AnyCreatedOrRemoved<TComponent>>(ReactiveCompositeLayer.AnyEventId);
+    }
+}
